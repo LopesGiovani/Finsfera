1897ace460ee1e42d01c4c2facd15eebd8cc64bf
Giovani <giovanilopesmcp@gmail.com>
2025-02-27 23:16:27 -0300

    dependecias

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+// Cria uma instância do QueryClient fora do componente para evitar recriações
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
